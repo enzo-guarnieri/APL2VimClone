efc2d6d716309001fc4d70f611b21f3412241ac0
enzo-guarnieri <63428392+enzo-guarnieri@users.noreply.github.com>
2023-11-11 12:00:11 -0300

    Update Main.java
    just add a few things and coments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,19 @@ import java.io.IOException;
 import java.util.Scanner;
 
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
+    	// list is the principal area for writing 
+    	// unmodifiedList is just the fist instance of the text (for the command :q! basically)
+    	// transferArea is a list for tranfer data (like commands :v, :y, :c, :p, etc.)
         doubleLinkedList list = null;
+        doubleLinkedList unmodifiedList = null;
+        doubleLinkedList transferArea = null;
 
         Scanner input = new Scanner(System.in);
         String command = "i";
+        
         while(!command.equals("o")) {
             System.out.println("digite seu comando:");
             command = input.nextLine();
@@ -17,6 +24,10 @@ public class Main {
             if (command.startsWith(":e")) {
                 try {
                     String[] commandContent = command.split(" ");
+                    if(commandContent.length != 1) {
+                    	System.out.println("Numero de argumentos diferente do que esperado, utilize o comando \":help \" para mais informações ");
+                    	break;
+                    }
 
                     list = fileOperations.transferFileDataToDoubleLinkedList(commandContent[1]);
                 }catch (FileNotFoundException e){
@@ -26,6 +37,10 @@ public class Main {
 
             if (command.startsWith(":w")) {
                 String[] commandContent = command.split(" ");
+                if(commandContent.length != 1) {
+                	System.out.println("Numero de argumentos diferente do que esperado, utilize o comando \":help \" para mais informações ");
+                	break;
+                }
 
                     if(list == null){
                         System.out.println("Lista ainda nao foi criada utilize o comando (:e) primeiro\n");
@@ -36,6 +51,7 @@ public class Main {
                     }
             }
         }
+        input.close();
         System.out.println("\nObrigado por usar nosso programa :)\n");
     }
 }
